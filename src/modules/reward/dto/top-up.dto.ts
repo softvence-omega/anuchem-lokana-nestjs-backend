dcc658b154f9c900e3e81e15eab4f6a02125a2b8
Shahin-Mia <shahin.cmt2@gmail.com>
2025-07-30 03:01:56 +0600

    topup dto done

--- src/modules/reward/dto/top-up.dto.ts
+++ src/modules/reward/dto/top-up.dto.ts
@@ -1,18 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsString } from "class-validator";
 
 export class TopUpDto {
+    @ApiProperty({ example: "+233241234567", type: "string" })
     @IsString()
     phoneNumber: string;
 
+    @ApiProperty({ example: 60.00, type: "number" })
     @IsNumber()
     amount: number;
 
+    @ApiProperty({ example: 617, type: "number" })
     @IsNumber()
     operatorId: number;
 
+    @ApiProperty({ example: "NG", type: "string" })
     @IsString()
     countryIsoName: string;
 
+    @ApiProperty({ example: 1000, type: "number" })
     @IsNumber()
     reward_points: number;
 }
